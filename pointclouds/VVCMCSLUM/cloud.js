18a2141d0b52045cf6e76d1919aa3a6c585868cc
marshainfosoft <marsha.infosoft@gmail.com>
2018-07-19 13:10:05 +0530

    Initial commit

---
+++ pointclouds/VVCMCSLUM/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 19988646,
+    "boundingBox": {
+        "lx": 274684.565826416,
+        "ly": 2149378.411743164,
+        "lz": 13.999542236328125,
+        "ux": 275654.1340637207,
+        "uy": 2150347.9799804689,
+        "uz": 983.5677795410156
+    },
+    "tightBoundingBox": {
+        "lx": 274684.566,
+        "ly": 2149378.412,
+        "lz": 14.0,
+        "ux": 275654.134,
+        "uy": 2150341.474,
+        "uz": 66.11
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 8.396707534790039,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
